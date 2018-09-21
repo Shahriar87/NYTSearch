ac321498a9eaa9a7293815f82534cb0b6dcd2870
Shahriar87 <shahriar.firoz@gmail.com>
2018-09-20 21:16:10 -0500

    further work on js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,32 @@ $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
+
+
+    // var article = $("#searchInput").text();
+
+    // var results = result.data;
+
+
+    // personImage.attr("src", results[i].images.fixed_height.url);
+
+    for (var i = 0; i < results.length; i++) {
+
+        var article = results.docs[i];
+
+        var articleImage = $("<img>");
+        articleImage.attr("src", result.docs[i].web_url)
+
+        console.log()
+
+
+    }
+
+
+
   console.log(result);
 }).fail(function(err) {
   throw err;
 });
+
 
